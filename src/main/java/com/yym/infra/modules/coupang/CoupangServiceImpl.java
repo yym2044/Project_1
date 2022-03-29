9d683d2b51f4c67a89ed6f1b6305cf77fe9a047a
yym2044 <yym2044@gmail.com>
2022-03-29 12:26:12 +0900

    .

--- src/main/java/com/yym/infra/modules/coupang/CoupangServiceImpl.java
+++ src/main/java/com/yym/infra/modules/coupang/CoupangServiceImpl.java
@@ -2,8 +2,15 @@ package com.yym.infra.modules.coupang;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CoupangServiceImpl implements CoupangService {
 
+	@Autowired
+	CoupangDao dao;
+		
 	@Override
 	public List<Coupang> selectList() throws Exception {
 		
